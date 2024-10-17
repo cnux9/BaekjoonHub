82eef4555e8cbc2b524b87014d704953fb710c67
cnux9 <65856226+cnux9@users.noreply.github.com>
2024-10-17 14:08:54 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int num) {
+        if(num%2==0) {
+            return "Even";//->안성재 쉐프
+        }else {
+            return "Odd";
+        }
+    }
+}
