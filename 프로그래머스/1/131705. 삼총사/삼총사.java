e3f8f53fc36489de98be2c252956a87b957d3dde
cnux9 <65856226+cnux9@users.noreply.github.com>
2024-11-18 12:20:08 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -5,9 +5,7 @@ class Solution {
             for (int j = i+1;j<number.length;j++) {
                 for (int k = j+1;k<number.length;k++) {
                     if (0==number[i]+number[j]+number[k] ) {
-                        if (i!=j && i!=k && j!=k) {
-                            count++;
-                        }
+                        count++;
                     }
                 }
             }
